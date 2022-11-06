2ad99c12a254df83d2745fb60e9ab547485de8c3
Ahad Rajput <sachem2015@gmail.com>
2022-11-06 01:58:56 -0500

    cleaned up api call to corestrike

--- TournamentScript.js
+++ TournamentScript.js
@@ -11,45 +11,45 @@ const sheets = file.SheetNames
 // Player Object for storing name + rating from Corestrike.gg
 const players = []
 
-//Fetch player data function
-
+// Fetch player data function
 async function getPlayerData (name) {
     var requestOptions = {
       method: 'GET',
       redirect: 'follow'
     };
 
-    const res = await fetch("https://corestrike.gg/lookup/Mystic?json=true", requestOptions);
+    const res = await fetch(`https://corestrike.gg/lookup/${name}?json=true`, requestOptions);
     const fetchResponse = await res.json();
-    // console.log(fetchResponse);
-    // return fetchResponse.rankedStats.rating_display;
     return Promise.resolve(fetchResponse.rankedStats);
 }
 
 async function runScript() {
+  // Taking info from excel sheet and putting it into data object
   for(let i = 0; i < sheets.length; i++)
   {
-    const temp = reader.utils.sheet_to_json(
-      file.Sheets[file.SheetNames[i]])
+    const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]])
     temp.forEach(async (res) => {
       data.push(res);
-      const playerName = data[i]['Please enter your account name exactly how it appears in game.\n\nI need to be able to check your rank on Corestrike.gg, if I cannot you will be disqualified'];
-      // console.log(playerName);
-      const playerStats = await getPlayerData(playerName);
-      // const playerRank = playerStats.rating_display;
-      // console.log(playerRank);
-      players.push(
-        {
-          Name: playerName,
-          Rating: playerStats.rating,
-          Rank: playerStats.rating_display
-        }        
-      ) 
-      console.log(players);
     })
   }
+  
+  // Pulling information from Corestrike.gg
+  for(let i = 0; i < data.length; i++){
+    const playerName = data[i]['Please enter your account name exactly how it appears in game.\n\nI need to be able to check your rank on Corestrike.gg, if I cannot you will be disqualified'];
+    const playerStats = await getPlayerData(playerName);
+    players.push(
+      {
+        Name: playerName,
+        Rating: playerStats.rating,
+        Rank: playerStats.rating_display,
+        Position: data[i]['What is your primary position in Omega Strikers?']
+      }        
+    ) 
+    // console.log(data);
+  }
+
+  console.log(players);
 }
-// Printing data
 
 await runScript();
 
